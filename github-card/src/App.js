0daf8c1beda52decf30250d32d1258443cb97ffa
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-10-03 23:55:14 -0500

    so, i gotta make the request within the app using componentDidMount... i was overcomplicating myself

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+  axios
+			.get('https://api.github.com/users/Purefallen11')
+    .then(res => {
+      this.setState({
+        ...this.state,
+        data: res.data
+      })
+      console.log(this.state.data)
+      })
+		.catch(err =>{ console.log(err)})
+  }
+
+  
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
 }
 
 export default App;
