15ad1cd6c32cb6ea9427bdcc518853577661079c
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-10-04 00:51:54 -0500

    adding followers

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -3,32 +3,48 @@ import React from 'react'
 import axios from 'axios'
 import UserCard from './components/UserCard';
 
+
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      data: []
+      data: [],
+      followers: []
     }
   }
-
+  
   componentDidMount() {
-  axios
-			.get('https://api.github.com/users/Purefallen11')
+    axios
+    .get('https://api.github.com/users/Purefallen11')
     .then(res => {
       this.setState({
         ...this.state,
         data: res.data
       })
-      console.log(this.state.data)
+      
+    })
+    .catch(err => { console.log(err) })
+    
+    axios
+    .get('https://api.github.com/users/Purefallen11/followers')
+    .then(res => {
+      this.setState({
+        ...this.state,
+        followers: res.data
       })
-		.catch(err =>{ console.log(err)})
+      console.log(this.state.followers)
+      })
+    .catch(err =>{console.log(err)})
   }
 
   
   render() {
     return (
-      <div>
-        <UserCard name={this.state.data.name} img={this.state.data.avatar_url} location={this.state.data.location} bio={this.state.data.bio} />
+      <div className='section'>
+          <div className='userCard'>
+            <UserCard name={this.state.data.name} img={this.state.data.avatar_url} location={this.state.data.location} bio={this.state.data.bio} />
+          </div>
+          
       </div>
     )
   }
--- github-card/src/components/UserCard.js
+++ github-card/src/components/UserCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import {
 Card, CardImg, CardText, CardBody,
-CardTitle, CardSubtitle, Button
+CardTitle, CardSubtitle
 } from 'reactstrap';
 import '../App.css'
 
 const UserCard = props => {
 	return (
-		<div className ='card'>
+		<div>
 		<Card>
-			<CardImg top width="100%" src={props.img} alt="Card image cap" />
+			<CardImg src={props.img} alt="Card image cap" />
 			<CardBody>
 				<CardTitle tag="h5">{props.name}</CardTitle>
 				<CardSubtitle tag="h6" className="mb-2 text-muted">{props.bio}</CardSubtitle>
